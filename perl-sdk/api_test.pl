845d5cac47fbaeb19449f1c1325cdfc7b18680ab
Luke Burgess <luke@lukestyle.com>
2013-01-27 16:25:40 +0000

    correct missing 'use api..' in test file

--- perl-sdk/api_test.pl
+++ perl-sdk/api_test.pl
@@ -6,14 +6,19 @@ use strict;
 # http://appppr.com/cgi-bin/ivc_api.pl
 # https://server2.lukeburgess.com/~appppr/cgi-bin/ivc_api.pl
 
-# open LOG, ">>/home/appppr/public_html/cgi-bin/gco.txt";
+use WWW::InventoryClerk::API;
 
 # todo: AuthId=apikey, AuthToken=secret
 
 my $ivc = new WWW::InventoryClerk::API( AuthId => 'bla',
 										AuthToken  => 'bla' );
 
+# open LOG, ">>/home/appppr/public_html/cgi-bin/gco.txt";
+
 # todo: namespace=booking, method=show
+
+# print qq~Content-type: text/html\r\n\r\n~;
+# print qq~Content-type: text/plain\r\n\r\n~;
 
 my $response = $ivc->GET('Booking');
 
